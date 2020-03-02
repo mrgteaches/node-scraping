3013121854e2b665b7b9d6933b9b41f3034b1328
David Giles <mrgteaches@live.com>
2020-03-02 09:48:31 -0500

    Basic functionality - one EP page

--- scraper.js
+++ scraper.js
@@ -1,13 +1,11 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 
-const siteUrl = "https://remoteok.io/";
+const siteUrl = "https://www.allinonehighschool.com/spanish-1-2018/";
 
-let siteName = "";
-const categories = new Set();
-const tags = new Set();
-const locations = new Set();
-const positions = new Set();
+const courseTitle = new Set();
+const content = new Set();
+const dayTitle = new Set();
 
 const fetchData = async () => {
   const result = await axios.get(siteUrl);
@@ -17,26 +15,47 @@ const fetchData = async () => {
 const getResults = async () => {
   const $ = await fetchData();
 
-  siteName = $('.top > .action-post-job').text();
+  // siteName = $('.top > .action-post-job').text();
+  siteName = "EP Scraper";
 
-  $(".tags .tag").each((index, element) => {
-    tags.add($(element).text());
+  // $(".tags .tag").each((index, element) => {
+  //   tags.add($(element).text());
+  // });
+  // $(".location").each((index, element) => {
+  //   locations.add($(element).text());
+  // });
+  // $("div.nav p").each((index, element) => {
+  //   categories.add($(element).text());
+  // });
+  // $('.company_and_position [itemprop="title"]').each((index, element) => {
+  //   positions.add($(element).text());
+  // });
+
+  var dayNo = "4";
+
+  // This is the version with changed html
+
+  // $("div.entry-content").each(function (index, element) {
+
+  //   dayTitle.add($(element).children("#day1").children("strong").text());
+  //   calculus.add($(element).children("#day" + dayNo).children("ol").text()); 
+  //   });
+
+  //This is the version with unchanged html
+  $("header.entry-header").each(function(index, element) {
+    courseTitle.add($(element).children("h1").text());
   });
-  $(".location").each((index, element) => {
-    locations.add($(element).text());
+
+  $("div.entry-content").each(function (index, element) {
+    dayTitle.add($(element).children("p").children("#day" + dayNo).text());
+    content.add($(element).children("p").children("#day" + dayNo).parent().next("ol").text());
   });
-  $("div.nav p").each((index, element) => {
-    categories.add($(element).text());
-  });
-  $('.company_and_position [itemprop="title"]').each((index, element) => {
-    positions.add($(element).text());
-  });
+
   return {
-    positions: [...positions].sort(),
-    tags: [...tags].sort(),
-    locations: [...locations].sort(),
-    categories: [...categories].sort(),
-    siteName,
+    courseTitle: [...courseTitle].sort(),
+    dayTitle: [...dayTitle].sort(),
+    content: [...content].sort(),
+    // siteName,
   };
 };
 
